6940c24f54146e8e911269cd6c761383f42e2063
Anderson dos Santos Cruz <andersondossantoscruz@Andersons-MacBook-Pro.local>
2021-07-19 16:31:50 -0300

    first tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const blueState = {
+  backgroundColor: 'MidnightBlue',
+  text: 'Change to Medium Violet Red',
+}
+
+const redState = {
+  backgroundColor: 'MediumVioletRed',
+  text: 'Change to Midnight Blue'
+}
+
+export function replaceCamelCaseWithBlankSpaces(colorName) {
+  return colorName.replace(/\B([A-Z])\B/g, ' $1')
+}
+
 function App() {
+  const [buttonState, setButtonState] = useState(redState)
+  const [isDisabled, toggleIsDisabled] = useState(false)
+  const {backgroundColor, text} = buttonState
+
+  const buttonStyle = { backgroundColor: isDisabled ? 'grey' : backgroundColor }
+
+  function handleOnClick() {
+    if (buttonState.backgroundColor === 'MediumVioletRed') {
+      setButtonState({...blueState})
+    }
+    else {
+      setButtonState({...redState})
+    }
+  }
+
+  function handleOnToggleCheckbox() {
+    toggleIsDisabled(isDisabled => !isDisabled)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{flex: 1, flexDirection: 'column'}}>
+      <button disabled={isDisabled} name="colorButton" style={buttonStyle} onClick={handleOnClick}>
+        {text}
+      </button>
+      <label htmlFor="disable-button-checkbox">Disable Button</label>
+      <input id="disable-button-checkbox" type="checkbox" checked={isDisabled} onChange={handleOnToggleCheckbox} />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,59 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from '@testing-library/react';
+import App, {replaceCamelCaseWithBlankSpaces} from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('button has correct initial color', () => {
+  render(<App />)
+  const colorButton = screen.getByRole('button', { name: 'Change to Midnight Blue'})
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'MediumVioletRed'})
+
+  fireEvent.click(colorButton)
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'MidnightBlue'})
+  expect(colorButton.textContent).toBe('Change to Medium Violet Red')
 });
+
+test('button is disabled when checkbox is checked', () => {
+  render (<App />)
+  const colorButton = screen.getByRole('button', { name: 'Change to Midnight Blue'})
+  expect(colorButton).toBeEnabled()
+
+  const checkbox = screen.getByRole('checkbox')
+  expect(checkbox).not.toBeChecked()
+
+  fireEvent.click(checkbox)
+
+  expect(checkbox).toBeChecked()
+  expect(colorButton).toBeDisabled()
+  expect(colorButton).toHaveStyle({ backgroundColor: 'grey' })
+})
+
+test('button is enabled when checkbox is unchecked', () => {
+  render (<App />)
+  const colorButton = screen.getByRole('button', { name: 'Change to Midnight Blue'})
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable Button'})
+
+  // Disable the button
+  fireEvent.click(checkbox)
+  expect(checkbox).toBeChecked()
+  expect(colorButton).not.toBeEnabled()
+  
+  // Enable the button
+  fireEvent.click(checkbox)
+  expect(checkbox).not.toBeChecked()
+  expect(colorButton).toBeEnabled()
+})
+
+describe('Spaces before camel-case',() => {
+  test('Works for no inner capital letter', () => {
+    expect(replaceCamelCaseWithBlankSpaces('Violet')).toBe('Violet')
+  })
+
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelCaseWithBlankSpaces('MidnightBlue')).toBe('Midnight Blue')
+  })  
+
+  test('Works for multiple inner capital letters', () => {
+    expect(replaceCamelCaseWithBlankSpaces('MediumVioletRed')).toBe('Medium Violet Red')
+  })    
+})
